d0bc1972fa78ab2915219b325f99534eae473bf3
Alexander <alexzubkoff123@gmail.com>
2023-02-18 19:17:03 +0200

    Created profile model for user p;an

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import Profile
 
 CustomUser = get_user_model()
 
@@ -14,3 +15,9 @@ class CustomUserAdmin(UserAdmin):
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'plan',]
+    raw_id_fields = ['user']
--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,28 @@
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 from django.db import models
 
 
 class CustomUser(AbstractUser):
     pass
+
+
+class Profile(models.Model):
+
+    class Plan(models.TextChoices):
+        BASIC = 'BA', 'Basic'
+        PREMIUM = 'PR', 'Premium'
+        ENTERPRISE = 'EN', 'Enterprise'
+
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    plan = models.CharField(
+        max_length=3,
+        choices=Plan.choices,
+        default=Plan.BASIC
+    )
+
+    def __str__(self):
+        return f'Profile of {self.user.username}'
